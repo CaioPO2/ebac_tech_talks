a0dfd74b8bb0f1a7f3e50109e32c2c4c737acad4
Caio <caiocesarpoke2@hotmail.com>
2024-11-04 17:21:54 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('Olá mundo');
+AOS.init();
+
+const dataDoEnvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEnvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteoEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60* 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteoEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteoEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteoEvento % horaEmMs) / minutoEmMs);
+    const segundoAteOEvento = Math.floor((distanciaAteoEvento % minutoEmMs)/ 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundoAteOEvento}s`;
+    
+    if(distanciaAteoEvento < 0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+},1000)
